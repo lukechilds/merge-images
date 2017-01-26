2edf7ff44c7161c1d3e4dafaf2566d458c8c541f
Luke Childs <lukechilds123@gmail.com>
2017-01-26 17:19:53 +0700

    Add basic logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-const imageMerge = () => true;
+// Return Promise
+const imageMerge = (sources = []) => new Promise(resolve => {
+	// Create canvas
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
+
+	// Load sources
+	const images = sources.map(src => new Promise(resolve => {
+		const img = new Image();
+		img.onload = () => resolve(img);
+		img.src = src;
+	}));
+
+	// When sources have loaded
+	Promise.all(images)
+		.then(images => {
+			// Draw images to canvas
+			images.forEach(img => ctx.drawImage(img, 0, 0));
+
+			// Resolve data uri
+			resolve(canvas.toDataURL());
+		});
+});
 
 module.exports = imageMerge;
