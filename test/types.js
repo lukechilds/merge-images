510b5c411c63860f53c0fa7909415d8bb9370f84
Luke Childs <lukechilds123@gmail.com>
2017-02-07 13:54:07 +0700

    Check mergeImages returns a Promise

--- test/types.js
+++ test/types.js
@@ -1,6 +1,11 @@
 import test from 'ava';
+import Canvas from 'canvas';
 import mergeImages from '../';
 
 test('mergeImages is a function', t => {
 	t.is(typeof mergeImages, 'function');
+});
+
+test('mergeImages returns a Promise', t => {
+	t.true(mergeImages([], {Canvas}) instanceof Promise);
 });
