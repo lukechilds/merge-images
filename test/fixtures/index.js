d45d9d7e516ce88219b888ffb14a41a9dedacff5
Luke Childs <lukechilds123@gmail.com>
2017-02-10 11:46:45 +0700

    Test mergeImages correctly merges images

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -5,7 +5,8 @@ module.exports = fixtures = {
   images: {
     body: 'body.png',
     eyes: 'eyes.png',
-    mouth: 'mouth.png'
+    mouth: 'mouth.png',
+    face: 'face.png'
   },
   getImage: image => pify(fs.readFile)(`${__dirname}/${fixtures.images[image]}`)
 }
--- test/unit.js
+++ test/unit.js
@@ -1,8 +1,20 @@
 import test from 'ava';
 import Canvas from 'canvas';
+import fixtures from './fixtures';
 import mergeImages from '../';
 
 test('mergeImages returns empty b64 string if nothing is passed in', async t => {
 	t.plan(1);
 	await mergeImages([], { Canvas }).then(b64 => t.is(b64, 'data:,'));
+});
+
+test('mergeImages correctly merges images', async t => {
+	t.plan(1);
+	const images = await Promise.all(['body', 'mouth', 'eyes'].map(image => fixtures.getImage(image)));
+	const b64 = await mergeImages(images, { Canvas });
+
+	const face = await fixtures.getImage('face');
+	const expectedB64 = `data:image/png;base64,${face.toString('base64')}`;
+
+	t.is(b64, expectedB64);
 });
