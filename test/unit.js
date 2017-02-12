f8ed05d46b41b44e88f7802a852a9523a1e6c269
Luke Childs <lukechilds123@gmail.com>
2017-02-12 19:03:18 +0700

    Test mergeImages returns correct data URI

--- test/unit.js
+++ test/unit.js
@@ -8,6 +8,16 @@ test('mergeImages returns empty b64 string if nothing is passed in', async t => 
 	await mergeImages([], { Canvas }).then(b64 => t.is(b64, 'data:,'));
 });
 
+test('mergeImages returns correct data URI', async t => {
+	t.plan(1);
+	const image = await fixtures.getImage('face.png');
+	const b64 = await mergeImages([image], { Canvas });
+
+	const expectedB64 = await fixtures.getDataURI('face.png');
+
+	t.is(b64, expectedB64);
+});
+
 test('mergeImages correctly merges images', async t => {
 	t.plan(1);
 	const images = await Promise.all(['body.png', 'mouth.png', 'eyes.png'].map(image => fixtures.getImage(image)));
