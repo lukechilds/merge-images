77801baca67719eb93da18f8251d2e050d05e730
Luke Childs <lukechilds123@gmail.com>
2017-02-14 16:04:05 +0700

    Test custom formats

--- test/unit.js
+++ test/unit.js
@@ -18,6 +18,23 @@ test('mergeImages returns correct data URI', async t => {
 	t.is(b64, expectedB64);
 });
 
+test('mergeImages encodes custom image formats', async t => {
+	const image = await fixtures.getImage('face.png');
+	const formats = ['png', 'jpeg'];
+	t.plan(formats.length);
+
+	const tests = formats.map(async format => {
+		const b64 = await mergeImages([image], {
+			format: `image/${format}`,
+			Canvas: Canvas
+		});
+		const expectedB64 = await fixtures.getDataURI(`face.${format}`);
+		t.is(b64, expectedB64);
+	});
+
+	await Promise.all(tests);
+});
+
 test('mergeImages correctly merges images', async t => {
 	t.plan(1);
 	const images = await Promise.all(['body.png', 'mouth.png', 'eyes.png'].map(image => fixtures.getImage(image)));
