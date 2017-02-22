4a0fa2527ad0a5765a8f846f7723158037e0520a
Luke Childs <lukechilds123@gmail.com>
2017-02-22 21:31:55 +0700

    Test mergeImages rejects Promise if node-canvas instance isn\'t passed in

--- test/errors.js
+++ test/errors.js
@@ -3,6 +3,11 @@ import Canvas from 'canvas';
 import mergeImages from '../';
 import fixtures from './fixtures';
 
+test('mergeImages rejects Promise if node-canvas instance isn\'t passed in', async t => {
+	t.plan(1);
+	t.throws(mergeImages([]));
+});
+
 test('mergeImages rejects Promise if image load errors', async t => {
 	t.plan(1);
 	t.throws(mergeImages([1], { Canvas }));
