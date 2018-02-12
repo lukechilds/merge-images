11db778de914c2f4e1998d04ba915eb6d1c7a2ff
Marc Steller <ich@marc-steller.de>
2018-02-13 00:17:22 +0100

    fixed linting

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ const mergeImages = (sources = [], options = {}) => new Promise(resolve => {
 			// Draw images to canvas
 			images.forEach(image => {
 				ctx.globalAlpha = image.opactiy ? image.opactiy : 1;
-				return ctx.drawImage(image.img, image.x || 0, image.y || 0)
+				return ctx.drawImage(image.img, image.x || 0, image.y || 0);
 			});
 
 			if (options.Canvas && options.format === 'image/jpeg') {
